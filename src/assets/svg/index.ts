fa3f16faa9aa84ef4291173f71d89d0d138d1cfc
aymore_leal <sergiojunioleal@hotmail.com>
2024-01-07 22:26:00 -0300

    [refactor] main exportation svg

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -4,6 +4,7 @@ import BlobPinkBright from './BlobPinkBright.svg';
 import BlobPurpleBottom from './BlobPurpleBottom.svg';
 import BlobYellow from './BlobYellow.svg';
 import Instances from './Instances.svg';
+import MessageAdd from './MessageAdd.svg';
 
 export {
   BlobBlueTop,
@@ -11,5 +12,6 @@ export {
   BlobPinkBright,
   BlobPurpleBottom,
   BlobYellow,
-  Instances
+  Instances,
+  MessageAdd
 }
