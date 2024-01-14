6bc8171cdc8f1b49780fbb3f7395349cd9dee5bc
aymore_leal <sergiojunioleal@hotmail.com>
2024-01-14 10:00:39 -0300

    [refactor] handling login failures

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,49 +1,57 @@
 import React from 'react';
 
-import { FormLoginProps } from './types';
-import { useChat } from '../../context';
-import { api } from '../../services';
-import { Button } from '../../common';
-
-import { BlobBlueTop, BlobBlue, BlobPinkBright, BlobPurpleBottom, BlobYellow } from '../../assets/svg';
+import { api } from '@services';
+import { Button } from '@common';
+import { useChat } from '@context';
+import { FormProps } from '@interface';
 
 import * as Styles from './styles';
+
+interface StatusProps {
+  type: 'standard' | 'error' | 'success';
+  title: string;
+}
 
 export default function Login(){
   const { setData } = useChat();
 
   const [loading, setLoading] = React.useState(false);
+  const [status, setStatus] = React.useState<StatusProps>({ type: 'standard', title: 'login' });
 
-  const onSubmit = async(evt: FormLoginProps) => {
+  const onSubmit = async (evt: FormProps) => {
     evt.preventDefault();
+
+    setStatus({ ...status, type: 'standard' })
+
     setLoading(true);
 
-    api.post('/login-user', {
-      name: evt.target['name'].value,
-      number: evt.target['number'].value
+    return api.post('/login-user', {
+      nickname: evt.target['nickname'].value,
+      password: evt.target['password'].value
     })
-      .then(success => setData(success.data))
-      .finally(() => setLoading(false))
+      .then(({ data }) => setData(data))
+      .catch((error) => {
+        if (error.response.status === 404) {
+          setStatus({ type: 'error', title: 'retry' })
+        }
+      })
+      .finally(() => setLoading(loading))
   };
 
   return (
     <Styles.Container>
-
-      <Styles.Background>
-        <img id='blob-purple-bottom' src={BlobPurpleBottom} alt='' />
-        <img id='blob-blue-top' src={BlobBlueTop} alt='' />
-        <img id='blob-blue' src={BlobBlue} alt='' />
-        <img id='blob-yellow' src={BlobYellow} alt='' />
-        <img id='blob-pink-bright' src={BlobPinkBright} alt='' />
-        <img id='blob-orange' src='https://dashboard.render.com/static/media/blob-orange.e3aa62395ea58419906ab40a537b585d.svg' alt='' />
-      </Styles.Background>
-
       <form method='POST' onSubmit={onSubmit}>
 
-        <input type='text' name='name' defaultValue='serginho' placeholder='Create an instance' />
-        <input type='text' name='number' defaultValue='553175564133' placeholder='Create an instance' />
+        <input type='text' name='nickname' defaultValue='dyson._.mori' placeholder='nickname' />
+        <input type='text' name='password' defaultValue='123456' placeholder='number' />
 
-        <Button type='submit' title='login' isLoading={loading} />
+        <Button
+          type='submit'
+          disabled={loading}
+          isLoading={loading}
+          standard={status.type}
+          title={status.title}
+        />
 
       </form>
     </Styles.Container>
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -1,42 +1,4 @@
-import { css, styled } from 'styled-components';
-
-export const Background = styled.div`
-  display: flex;
-  position: absolute;
-
-  width: 100%;
-  height: 100vh;
-
-  #blob-purple-bottom, #blob-blue-top, #blob-yellow, #blob-pink-bright, #blob-orange {
-    position: absolute;
-
-    width: 500px;
-    height: 500px;
-  }
-
-  #blob-purple-bottom {
-    bottom: 0;
-    right: 0;
-  }
-
-  #blob-blue-top {
-    top: 0;
-    right: 0;
-  }
-
-  #blob-yellow {
-    right: 0;
-  }
-
-  #blob-pink-bright {
-    bottom: 0;
-    left: 0;
-  }
-
-  #blob-orange {
-    bottom: 0;
-  }
-`;
+import { styled } from 'styled-components';
 
 export const Container = styled.main`
   display: flex;
